65f4ff27be93669c4b924a0d1b0f6666b3fe5615
Collin McIntyre <crm11396@gmail.com>
2021-04-27 19:52:37 -0500

    strip out old code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,28 +2,16 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-import frc.robot.commands.DeployPickup;
-import frc.robot.commands.DriveWithJoystick;
-
 public class RobotContainer 
 {
     public RobotContainer() 
     {
-        Hardware.configureHardware();
-
         configureButtonBindings();
     }
 
     private void configureButtonBindings() 
     {
-        DeployPickup deployPickup = new DeployPickup(Hardware.Pickup.instance);
-
-        Hardware.Drive.instance.setDefaultCommand(new DriveWithJoystick(Hardware.Drive.instance, 
-                                                                        () -> Hardware.Controls.Joysticks.DRIVE.getY(),
-                                                                        () -> Hardware.Controls.Joysticks.DRIVE.getX()));
         
-        Hardware.Controls.Buttons.CO_DRIVE_BUTTON_6.whenPressed(deployPickup);
-        Hardware.Controls.Buttons.CO_DRIVE_BUTTON_7.cancelWhenPressed(deployPickup);
     }
 
     public Command getAutonomousCommand() 
