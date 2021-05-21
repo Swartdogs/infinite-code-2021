b6874053d8461b3b410e7881355e3f60cf880449
Collin McIntyre <crm11396@gmail.com>
2021-05-20 20:15:04 -0500

    RobotLog

---
+++ src/test/java/frc/robot/subsystems/RobotLogTest.java
@@ -0,0 +1,51 @@
+package frc.robot.subsystems;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import frc.robot.abstraction.SwartdogCommand;
+import frc.robot.abstraction.SwartdogSubsystem;
+import frc.robot.subsystems.RobotLog;
+import frc.robot.subsystems.RobotLog.CommandMode;
+import frc.robot.subsystems.RobotLog.GameMode;
+
+public class RobotLogTest 
+{
+    private class CmdExample extends SwartdogCommand
+    {
+
+    }
+
+    @Test
+    public void testLogWrite()
+    {
+        SwartdogSubsystem.REGISTER_SUBSYSTEMS = false;
+
+        RobotLog logSubsystem = new RobotLog("test.csv", (() -> 1));
+        CmdExample cmd        = new CmdExample();
+
+        logSubsystem.periodic();
+        logSubsystem.periodic();
+        logSubsystem.periodic();
+
+        logSubsystem.logGameMode(GameMode.Autonomous);
+
+        logSubsystem.periodic();
+        logSubsystem.periodic();
+
+        logSubsystem.logCommand(cmd, CommandMode.Start, new HashMap<String, String>());
+
+        logSubsystem.periodic();
+        logSubsystem.periodic();
+
+        logSubsystem.logCommand(cmd, CommandMode.End, new HashMap<String, String>());
+
+        logSubsystem.periodic();
+        logSubsystem.periodic();
+        logSubsystem.periodic();
+
+        logSubsystem.logGameMode(GameMode.Disabled);
+
+    }
+}
