a2ba7fb076729fae8d521a3cd850d5d5c7532e73
Collin McIntyre <crm11396@gmail.com>
2021-06-01 00:51:32 -0500

    remove swerve constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,47 +2,34 @@ package frc.robot;
 
 import java.util.function.DoubleUnaryOperator;
 
-public final class Constants 
+public final class Constants
 {
-    public static final double FL_MODULE_X            = 0;
-    public static final double FL_MODULE_Y            = 0;
-    public static final double FL_MODULE_OFFSET       = 0;
-    public static final double FR_MODULE_X            = 0;
-    public static final double FR_MODULE_Y            = 0;
-    public static final double FR_MODULE_OFFSET       = 0;
-    public static final double BL_MODULE_X            = 0;
-    public static final double BL_MODULE_Y            = 0;
-    public static final double BL_MODULE_OFFSET       = 0;
-    public static final double BR_MODULE_X            = 0;
-    public static final double BR_MODULE_Y            = 0;
-    public static final double BR_MODULE_OFFSET       = 0;
+    public static final double              LIMELIGHT_LED_OFF       = 1;
+    public static final double              LIMELIGHT_LED_ON        = 3;
 
-    public static final double LIMELIGHT_LED_OFF      = 1;
-    public static final double LIMELIGHT_LED_ON       = 3;
+    public static final double              TARGET_HEIGHT           = 0;
+    public static final double              LIMELIGHT_HEIGHT        = 0;
+    public static final double              TARGET_HEIGHT_DELTA     = TARGET_HEIGHT - LIMELIGHT_HEIGHT;
 
-    public static final double TARGET_HEIGHT          = 0;
-    public static final double LIMELIGHT_HEIGHT       = 0;
-    public static final double TARGET_HEIGHT_DELTA    = TARGET_HEIGHT - LIMELIGHT_HEIGHT;
+    public static final double              LIMELIGHT_ANGLE         = 0;
 
-    public static final double LIMELIGHT_ANGLE        = 0;
+    public static final double              MOTOR_MOTION_THRESHOLD  = 0.02;
+    public static final double              HANGER_MIN_POSITION     = -3755;
+    public static final double              HANGER_MAX_POSITION     = -1780;
 
-    public static final double MOTOR_MOTION_THRESHOLD = 0.02;
-    public static final double HANGER_MIN_POSITION    = -3755;
-    public static final double HANGER_MAX_POSITION    = -1780;
+    public static final int                 MAX_BALL_COUNT          = 5;
 
-    public static final int    MAX_BALL_COUNT         = 5;
+    public static final double              PICKUP_SPEED            = 0.62;
+    public static final double              PICKUP_CLOG_TIME        = 2;
+    public static final double              PICKUP_STOW_DELAY       = 1;
 
-    public static final double PICKUP_SPEED           = 0.62;
-    public static final double PICKUP_CLOG_TIME       = 2;
-    public static final double PICKUP_STOW_DELAY      = 1;
+    public static final double              BALLPATH_JAM_TIME       = 1.2;
+    public static final double              BALLPATH_RAMP_MIN       = 0.02;
+    public static final double              BALLPATH_RAMP_STEP      = 0.10;
+    public static final double              BALLPATH_SPEED          = 0.7;
 
-    public static final double BALLPATH_JAM_TIME      = 1.2;
-    public static final double BALLPATH_RAMP_MIN      = 0.02;
-    public static final double BALLPATH_RAMP_STEP     = 0.10;
-    public static final double BALLPATH_SPEED         = 0.7;
+    public static final int                 SHOOTER_RPM_DEADBAND    = 50;
 
-    public static final int SHOOTER_RPM_DEADBAND      = 50;
-
-    public static final DoubleUnaryOperator HOOD_LOOKUP = DoubleUnaryOperator.identity();
-    public static final DoubleUnaryOperator SHOOTER_LOOKUP = DoubleUnaryOperator.identity();
+    public static final DoubleUnaryOperator HOOD_LOOKUP             = DoubleUnaryOperator.identity();
+    public static final DoubleUnaryOperator SHOOTER_LOOKUP          = DoubleUnaryOperator.identity();
 }
